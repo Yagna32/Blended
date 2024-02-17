4d59ae4e18b06cafcf101800e7e1f9daa62bdf25
yagna <yagnapatelhirenk@gmail.com>
2024-02-17 22:54:53 +0530

    Links in breadcrums

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './Breadcrum.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 const Breadcrum = (props) => {
     const {product} = props;
   return (
     <div className='breadcrum'>
-        <p>HOME</p> <img src={arrow_icon} alt="" />
-        <p>SHOP</p> <img src={arrow_icon} alt="" />
-        <p>{product.category}</p> <img src={arrow_icon} alt="" />
+        <Link to='/' className='link'><p>Shop</p></Link> <img src={arrow_icon} alt="" />
+        <Link to='/women' className='link'><p>{product.category}</p></Link> <img src={arrow_icon} alt="" />
         <p>{product.name}</p>
     </div>
   )
