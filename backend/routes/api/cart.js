a98d3c373aef1a880df271fb62ef107910871326
yagna <yagnapatelhirenk@gmail.com>
2024-02-24 12:48:08 +0530

    Implemented Redis cache on getCart API

--- backend/routes/api/cart.js
+++ backend/routes/api/cart.js
@@ -2,41 +2,63 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/Users')
 const {Authenticate} = require('../../middlewares/tempAuth')
+const {Redis} = require('ioredis')
+
+const redisClient = new Redis()
+
+const getUserCachedCart= (req,res,next)=>{
+    const email = req.user.email;
+    redisClient.get(email,(err,data)=>{
+        if(err) throw err;
+        if(data !== null) {
+            console.log(data)
+            res.send(JSON.parse(data))
+        }
+        else {
+            next();
+        }
+    })
+}
 
 router.post('/addtoCart',Authenticate,async(req,res)=>{
-    const userCartData = await User.findOneAndUpdate({email:req.user.email},
-            {$push: {cartData: {product_id: req.body.itemId,price:req.body.price}}},
-            {new:true}
-            )
-            console.log(userCartData.cartData)
-    res.send(userCartData.cartData)
+    const updatedUser = await User.findOneAndUpdate({email:req.user.email},
+        {$push: {cartData: {product_id: req.body.itemId,price:req.body.price}}},
+        {new:true}
+    )
+    console.log(updatedUser.cartData)
+    
+    redisClient.setex(updatedUser.email,60,JSON.stringify(updatedUser.cartData));
+
+    res.send(updatedUser.cartData)
 })  
 
 
 router.post('/removeFromCart',Authenticate,async(req,res)=>{
-    let checkQuantity = await User.findOne({email:req.user.email,cartData: {product_id: req.body.itemId,price:req.body.price}})
-    if(checkQuantity && checkQuantity.cartData.length > 0){
-        checkQuantity = checkQuantity.cartData;
-        checkQuantity.pop();
+    let userCart = await User.findOne({email:req.user.email,cartData: {product_id: req.body.itemId,price:req.body.price}})
+    if(userCart && userCart.cartData.length > 0){
+        userCart = userCart.cartData;
+        userCart.pop();
     }
-    console.log(checkQuantity)
-    if(checkQuantity <= 0){
+    console.log(userCart)
+    if(userCart <= 0){
         userData = await User.findOneAndUpdate(
             {email:req.user.email},{cartData:[]},{new:true}
             )
     }
     else {
         userData = await User.findOneAndUpdate(
-            {email:req.user.email},{cartData:checkQuantity},{new:true}
+            {email:req.user.email},{cartData:userCart},{new:true}
             )
     }
+    redisClient.setex(userData.email,60,JSON.stringify(userData.cartData));
     res.send(userData.cartData);
 })
 
-router.get('/getCart',Authenticate,async(req,res)=>{
+router.get('/getCart',Authenticate,getUserCachedCart,async(req,res)=>{
     let userData = await User.findOne({email: req.user.email})
     if(userData)
     {
+        redisClient.setex(userData.email,60,JSON.stringify(userData.cartData))
         res.json(userData.cartData)
         return;
     }
