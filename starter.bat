5b795e8cddbc6bf894417b51931ef6214212e559
yagna <yagnapatelhirenk@gmail.com>
2024-03-03 00:46:26 +0530

    single starter file for project

---
+++ starter.bat
@@ -0,0 +1,11 @@
+rem "Starting Backend"
+cd blended-server
+start npm run dev 
+
+rem "Starting Frontend"
+cd ../blended-client
+start npm run start  
+
+rem "Starting Admin"
+cd ../blended-admin
+start npm run dev
